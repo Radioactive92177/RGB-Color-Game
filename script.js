b9cff79b8f8b727894d145c5c3360f5c221949c3
RadioActive92177 <Raju.11811924@lpu.in>
2020-08-04 02:20:47 +0530

    Coloring of Panels with Random color done

---
+++ script.js
@@ -0,0 +1,45 @@
+// function to generate random rgb values
+const generateRandomColors = (num) => {
+  let colorArray = [];
+
+  for (let i = 0; i < num; i++) {
+    let r = Math.floor(Math.random() * 256 + 0); // red
+    let g = Math.floor(Math.random() * 256 + 0); // blue
+    let b = Math.floor(Math.random() * 256 + 0); // green
+
+    let rgb = "rgb(" + r + ", " + g + ", " + b + ")";
+    colorArray.push(rgb);
+  }
+  return colorArray;
+};
+
+// function to pick random color
+const pickColor = (colorArray) => {
+  let randomColor = Math.floor(Math.random() * colorArray.length);
+  return colorArray[randomColor];
+};
+
+// function for changes if answer is CORRECT!
+const onCorrectAnswer = (color, panels, message, reset) => {
+  for (let i = 0; i < numPanels; i++) {
+    panels[i].style.background = color;
+  }
+  message.textContent = "Correct!"
+  reset.textContent = "Play Again?"
+};
+
+// function for changes if answer is WRONG!
+const onWrongAnswer = (panel, message) => {
+    panel.style.background = "#232323";
+    message.textContent = "Try Again!"
+}
+
+// On Start
+let panels = document.querySelectorAll(".panels");
+let numPanels = panels.length;
+
+colors = generateRandomColors(numPanels);
+
+for (let i = 0; i<numPanels; i++){
+    panels[i].style.background = colors[i];
+}
